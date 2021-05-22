a623157e98624502215328dd3778510a5333d397
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-05-22 16:20:13 -0300

    feat: add AppError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,32 @@
-console.log('teste');
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '../errors/AppError';
+
+const app = express();
+const port = 3333;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, nextFunction: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
