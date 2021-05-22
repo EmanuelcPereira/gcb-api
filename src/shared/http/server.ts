98ca79d0b4ed8f4e2a5c82c6044065c6a56e7702
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-05-22 18:26:30 -0300

    chore: add tsconfig-paths

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,7 +2,8 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 const port = 3333;
